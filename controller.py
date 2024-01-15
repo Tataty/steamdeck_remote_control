9e6cc82193b3fd554835fac8e0b4ec924d6eafca
Tataty <ruslanl64kv@gmail.com>
2024-01-15 22:23:25 +0200

    test3 controller

--- controller.py
+++ controller.py
@@ -1,133 +1,99 @@
-import sys
-import os, struct, array
-import logging
-import time
-import queue
-from fcntl import ioctl
-import threading
-from ttypes import ButtonID, AxisID
-from typing import List
+# App with a green ball in the center that moves when you press the HAT buttons
+import pyjoystick
+from pyjoystick.sdl2 import Key, Joystick, run_event_loop
+# from pyjoystick.pygame import Key, Joystick, run_event_loop
+from qt_thread_updater import ThreadUpdater
+
+from qtpy import QtWidgets, QtGui, QtCore
 
 
-logger = logging.getLogger(__name__)
+app = QtWidgets.QApplication()
 
+updater = ThreadUpdater()
 
-class SteamDeckController:
+main = QtWidgets.QWidget()
+main.setLayout(QtWidgets.QHBoxLayout())
+main.resize(800, 600)
+main.show()
 
-    dev_loc = "/dev/input/"
+lbl = QtWidgets.QLabel()  # Absolute positioning
+main.layout().addWidget(lbl, alignment=QtCore.Qt.AlignTop)
 
-    axis_map = [
-        AxisID.LX,
-        AxisID.LY,
-        AxisID.THROTTLE_L,
-        AxisID.RX,
-        AxisID.RY,
-        AxisID.THROTTLE_R,
-        AxisID.PAD_X,
-        AxisID.PAD_Y
-    ]
+mover = QtWidgets.QLabel(parent=main)  # Absolute positioning
+mover.resize(50, 50)
+mover.point = main.rect().center()
+mover.move(mover.point)
+mover.show()
 
-    button_map = [
-        ButtonID.A,
-        ButtonID.B,
-        ButtonID.X,
-        ButtonID.Y,
-        ButtonID.L2,
-        ButtonID.R2,
-        ButtonID.SELECT, # TODO change to View button
-        ButtonID.START,  # TODO change to Menu button
-        ButtonID.DUMMY,  # Real button should be here! But which? 
-        ButtonID.THUMBL,
-        ButtonID.THUMBR,
-    ]
+def svg_paint_event(self, event):
+    painter = QtGui.QPainter(self)
+    painter.setRenderHint(painter.Antialiasing, True)
 
-    def __init__(self, collect_logs= False) -> None:
-        self.fn: str = self.await_controller()
+    # Get Black Background
+    rect = self.rect()
+    center = rect.center()
+    radius = 20
 
-        print("Connecting to controller")
-        self.jsdev = open(self.fn, 'rb')
+    # Colors
+    painter.setBrush(QtGui.QColor('green'))  # Fill color
+    painter.setPen(QtGui.QColor('black'))  # Line Color
 
-        buf = array.array('B', [0] * 64)
-        ioctl(self.jsdev, 0x80006a13 + (0x10000 * len(buf)), buf)
-        self.js_name = buf.tobytes().rstrip(b'\x00').decode('utf-8')
-        print('Device name: {}'.format(self.js_name))
+    # Draw
+    painter.drawEllipse(center, radius, radius)
 
-        self.collect_logs = collect_logs
-        self.log_queue = queue.Queue()
-        self.current_state = {
-            "axises": {},
-            "buttons": {},
-        }
-        self.previous_state = {
-            "axises": {},
-            "buttons": {},
-        }
-        self.monitor_thread = threading.Thread(target=self._monitor_dev)
-        self.monitor_dev()
+    painter.end()
 
-    def await_controller(self) -> str:
-        print("Waiting for controller")
-        while True:
-            for fn in os.listdir(self.dev_loc):
-                if fn.startswith('js'):
-                    print( 'Controller found: {}{}'.format(self.dev_loc, fn))
-                    return f"{self.dev_loc}{fn}"
-                else:
-                    print("Controller not found")
-                    time.sleep(1)
+mover.paintEvent = svg_paint_event.__get__(mover, mover.__class__)
 
+def handle_key_event(key):
+    updater.now_call_latest(lbl.setText, '{}: {} = {}'.format(key.joystick, key, key.value))
 
-    def monitor_dev(self) -> None:
-        self._terminate_monitor: bool = False
-        self.monitor_thread.start()
+    # print(key, '-', key.keytype, '-', key.number, '-', key.value)
 
-
-    def _monitor_dev(self, vibrate: bool = False) -> None:   # TODO vibrate not implemented
-        while True:
-            if self._terminate_monitor:
-                return
-            self.evbuf = self.jsdev.read(8)
-            if self.evbuf:
-                time1, value, type, number = struct.unpack('IhBB', self.evbuf)
-                if type & 0x01:
-                    print("= {} {} -> {}".format(value, number, self.button_map[number]))
-                    self.current_state["buttons"][self.button_map[number]] = value
-
-                elif type & 0x02:
-                    if number != 6 and number != 7:
-                        self.current_state["axises"][self.axis_map[number]] = value
-                        print("{}: {}".format(self.axis_map[number], value))
-                    elif number == 6 and value == -32767:
-                        # self.current_state["buttons"][] = 
-                        self.current_state["buttons"][ButtonID.PAD_LEFT] = 1 
-                    elif number == 6 and value == 32767:
-                        self.current_state["buttons"][ButtonID.PAD_RIGHT] = 1
-                    elif number ==6 and value == 0:
-                        self.current_state["buttons"][ButtonID.PAD_LEFT] = 0
-                        self.current_state["buttons"][ButtonID.PAD_RIGHT] = 0
-                    elif number == 7 and value == -32767:
-                        self.current_state["buttons"][ButtonID.PAD_UP] = 1
-                    elif number == 7 and value == 32767:
-                        self.current_state["buttons"][ButtonID.PAD_DOWN] = 1
-                    elif number == 7 and value == 0:
-                        self.current_state["buttons"][ButtonID.PAD_UP] = 0
-                        self.current_state["buttons"][ButtonID.PAD_DOWN] = 0
-
-    def send_update(self):
-        self.current_state_copy = self.current_state.copy()
-        for button_id, value in self.current_state_copy.get("buttons").items():
-            if value != self.previous_state.get("buttons").get(button_id, 0):
-                print(button_id, value)
-        for axis_id, value in self.current_state_copy.get("axises").items():
-            if value != self.previous_state.get("axises").get(axis_id, 0):
-                print(axis_id, value)
-        
-        self.previous_state["buttons"] = self.current_state_copy.get("buttons").copy()
-        self.previous_state["axises"] = self.current_state_copy.get("axises").copy()
-
-    def terminate(self) -> None:
-        self._terminate_monitor = True
-        self.monitor_thread.join()
+    if key.keytype != Key.HAT:
         return
-        
-sdk = SteamDeckController()
+
+    if key.value == Key.HAT_UP:
+        mover.point.setY(mover.point.y() - 10)
+    elif key.value == Key.HAT_DOWN:
+        mover.point.setY(mover.point.y() + 10)
+    if key.value == Key.HAT_LEFT:
+        mover.point.setX(mover.point.x() - 10)
+    elif key.value == Key.HAT_UPLEFT:
+        mover.point.setX(mover.point.x() - 5)
+        mover.point.setY(mover.point.y() - 5)
+    elif key.value == Key.HAT_DOWNLEFT:
+        mover.point.setX(mover.point.x() - 5)
+        mover.point.setY(mover.point.y() + 5)
+    elif key.value == Key.HAT_RIGHT:
+        mover.point.setX(mover.point.x() + 10)
+    elif key.value == Key.HAT_UPRIGHT:
+        mover.point.setX(mover.point.x() + 5)
+        mover.point.setY(mover.point.y() - 5)
+    elif key.value == Key.HAT_DOWNRIGHT:
+        mover.point.setX(mover.point.x() + 5)
+        mover.point.setY(mover.point.y() + 5)
+    updater.now_call_latest(mover.move, mover.point)
+
+# If it button is held down it should be repeated
+repeater = pyjoystick.HatRepeater(first_repeat_timeout=0.5, repeat_timeout=0.03, check_timeout=0.01)
+
+mngr = pyjoystick.ThreadEventManager(event_loop=run_event_loop,
+                                     handle_key_event=handle_key_event,
+                                     button_repeater=repeater)
+mngr.start()
+
+# Find key functionality
+btn = QtWidgets.QPushButton('Find Key:')
+
+def find_key():
+    key = mngr.find_key(timeout=float('inf'))
+    if key is None:
+        btn.setText('Find Key:')
+    else:
+        btn.setText('Find Key: {} = {}'.format(key, key.value))
+
+btn.clicked.connect(find_key)
+main.layout().addWidget(btn, alignment=QtCore.Qt.AlignTop)
+
+app.exec_()
